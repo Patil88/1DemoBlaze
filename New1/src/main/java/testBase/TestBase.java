672fae784314fee5b8ceb7334e5cbd9abc734bf8
Patil88 <omkarpharate15@gmail.com>
2022-11-02 20:36:24 +0530

    Project yet to be completed
    not completed yet

---
+++ New1/src/main/java/testBase/TestBase.java
@@ -0,0 +1,21 @@
+package testBase;
+
+import org.testng.annotations.Test;
+
+public class TestBase 
+{@Test
+	public void test1()
+	{
+		
+		System.out.println("login here to complete actions");
+	}
+
+	public void test11() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
---
+++ New1/src/test/java/testPages/Testpages.java
@@ -0,0 +1,17 @@
+package testPages;
+
+import org.testng.annotations.Test;
+
+import testBase.TestBase;
+
+public class Testpages extends TestBase {
+	
+@Test
+
+public static void main(String[args]) {public void testexecute()
+	{
+	TestBase t=new TestBase();
+	t.test1();
+	}
+}
+}
